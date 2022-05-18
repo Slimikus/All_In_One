4c06b57b3bc605f65436beaba004d524e2de194b
Slimikus <slimikus@gmail.com>
2022-05-18 22:37:26 +0300

    Initial Commit

---
+++ All_In_One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All_In_One
+//
+//  Created by Андрей Маковецкий on 18.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
